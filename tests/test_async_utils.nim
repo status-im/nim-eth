1cc52976df8fe2665e8e6caa311fbbb8df08f8d9
Zahary Karadjov <zahary@gmail.com>
2019-03-28 12:52:31 +0200

    Add a new asyncDiscard replacement that traces recoverable errors and aborts on defects

---
+++ tests/test_async_utils.nim
@@ -0,0 +1,26 @@
+# TODO: Make this part of the test suite.
+# We need to be able to test that a program fails in certain way.
+# The testing framework from Chronicles can be extracted in a separate package.
+
+import
+  chronos, ../eth/async_utils
+
+type
+  SomeRecoverableError = object of CatchableError
+  SomeDefect = object of Defect
+
+proc failingAsyncProc(err: ref Exception = nil) {.async.} =
+  await sleepAsync(0)
+  if err != nil:
+    raise err
+
+proc main {.async.} =
+  type Error =
+    # Exception
+    SomeDefect
+    # SomeRecoverableError
+  traceAsyncErrors failingAsyncProc(newException(Error, "some exception"))
+
+waitFor main()
+waitFor sleepAsync(2000)
+
