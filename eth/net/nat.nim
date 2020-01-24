1756f7b41f738df1e5775ede635cd9a1e0fcd990
Jacek Sieka <jacek@status.im>
2020-01-24 09:24:29 +0100

    result -> stew/result (#159)

--- eth/net/nat.nim
+++ eth/net/nat.nim
@@ -8,7 +8,7 @@
 
 import
   options, os, strutils, times,
-  result, nat_traversal/[miniupnpc, natpmp],
+  stew/result, nat_traversal/[miniupnpc, natpmp],
   chronicles, json_serialization/std/net
 
 type
