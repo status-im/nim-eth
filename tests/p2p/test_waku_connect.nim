c679b9d436fa1e5e00690dba443c0c9f31b321b9
kdeme <kim.demey@gmail.com>
2020-02-13 17:48:55 +0100

    Fix priority check of topic-interest versus bloom filter

--- tests/p2p/test_waku_connect.nim
+++ tests/p2p/test_waku_connect.nim
@@ -19,11 +19,12 @@ const
 # TODO: Just repeat all the test_shh_connect tests here that are applicable or
 # have some commonly shared test code for both protocols.
 suite "Waku connections":
-  asyncTest "Test Waku connections":
-    var n1 = setupTestNode(Waku)
-    var n2 = setupTestNode(Waku)
-    var n3 = setupTestNode(Waku)
-    var n4 = setupTestNode(Waku)
+  asyncTest "Waku connections":
+    var
+      n1 = setupTestNode(Waku)
+      n2 = setupTestNode(Waku)
+      n3 = setupTestNode(Waku)
+      n4 = setupTestNode(Waku)
 
     var topics: seq[Topic]
     n1.protocolState(Waku).config.topics = some(topics)
@@ -34,33 +35,64 @@ suite "Waku connections":
     n1.startListening()
     n3.startListening()
 
-    let p1 = await n2.rlpxConnect(newNode(initENode(n1.keys.pubKey, n1.address)))
-    let p2 = await n2.rlpxConnect(newNode(initENode(n3.keys.pubKey, n3.address)))
-    let p3 = await n4.rlpxConnect(newNode(initENode(n3.keys.pubKey, n3.address)))
+    let
+      p1 = await n2.rlpxConnect(newNode(initENode(n1.keys.pubKey, n1.address)))
+      p2 = await n2.rlpxConnect(newNode(initENode(n3.keys.pubKey, n3.address)))
+      p3 = await n4.rlpxConnect(newNode(initENode(n3.keys.pubKey, n3.address)))
     check:
       p1.isNil
       p2.isNil == false
       p3.isNil == false
 
-  asyncTest "Test Waku topic-interest":
-    var wakuTopicNode = setupTestNode(Waku)
-    var wakuNode = setupTestNode(Waku)
+  asyncTest "Waku topic-interest":
+    var
+      wakuTopicNode = setupTestNode(Waku)
+      wakuNode = setupTestNode(Waku)
 
-    let topic1 = [byte 0xDA, 0xDA, 0xDA, 0xAA]
-    let topic2 = [byte 0xD0, 0xD0, 0xD0, 0x00]
-    let wrongTopic = [byte 0x4B, 0x1D, 0x4B, 0x1D]
+    let
+      topic1 = [byte 0xDA, 0xDA, 0xDA, 0xAA]
+      topic2 = [byte 0xD0, 0xD0, 0xD0, 0x00]
+      wrongTopic = [byte 0x4B, 0x1D, 0x4B, 0x1D]
 
     wakuTopicNode.protocolState(Waku).config.topics = some(@[topic1, topic2])
 
     wakuNode.startListening()
-    await wakuTopicNode.peerPool.connectToNode(newNode(initENode(wakuNode.keys.pubKey,
-                                                            wakuNode.address)))
+    await wakuTopicNode.peerPool.connectToNode(newNode(
+      initENode(wakuNode.keys.pubKey, wakuNode.address)))
 
     let payload = repeat(byte 0, 10)
     check:
       wakuNode.postMessage(ttl = safeTTL, topic = topic1, payload = payload)
       wakuNode.postMessage(ttl = safeTTL, topic = topic2, payload = payload)
       wakuNode.postMessage(ttl = safeTTL, topic = wrongTopic, payload = payload)
+      wakuNode.protocolState(Waku).queue.items.len == 3
+    await sleepAsync(waitInterval)
+    check:
+      wakuTopicNode.protocolState(Waku).queue.items.len == 2
+
+  asyncTest "Waku topic-interest versus bloom filter":
+    var
+      wakuTopicNode = setupTestNode(Waku)
+      wakuNode = setupTestNode(Waku)
+
+    let
+      topic1 = [byte 0xDA, 0xDA, 0xDA, 0xAA]
+      topic2 = [byte 0xD0, 0xD0, 0xD0, 0x00]
+      bloomTopic = [byte 0x4B, 0x1D, 0x4B, 0x1D]
+
+    # It was checked that the topics don't trigger false positives on the bloom.
+    wakuTopicNode.protocolState(Waku).config.topics = some(@[topic1, topic2])
+    wakuTopicNode.protocolState(Waku).config.bloom = toBloom([bloomTopic])
+
+    wakuNode.startListening()
+    await wakuTopicNode.peerPool.connectToNode(newNode(
+      initENode(wakuNode.keys.pubKey, wakuNode.address)))
+
+    let payload = repeat(byte 0, 10)
+    check:
+      wakuNode.postMessage(ttl = safeTTL, topic = topic1, payload = payload)
+      wakuNode.postMessage(ttl = safeTTL, topic = topic2, payload = payload)
+      wakuNode.postMessage(ttl = safeTTL, topic = bloomTopic, payload = payload)
       wakuNode.protocolState(Waku).queue.items.len == 3
     await sleepAsync(waitInterval)
     check:
