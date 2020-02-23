cb27813f3b4f2936f3f8c61c4791273b0df66d33
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2020-02-23 01:53:57 +0100

    log the stack trace on fatal exceptions

--- eth/async_utils.nim
+++ eth/async_utils.nim
@@ -5,7 +5,7 @@ proc catchOrQuit(error: Exception) =
   if error of CatchableError:
     trace "Async operation ended with a recoverable error", err = error.msg
   else:
-    fatal "Fatal exception reached", err = error.msg
+    fatal "Fatal exception reached", err = error.msg, stackTrace = getStackTrace()
     quit 1
 
 proc traceAsyncErrors*(fut: FutureBase) =
