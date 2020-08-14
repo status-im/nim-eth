c712a642cd9630c1c4e8e911603d062c19be4e7d
Zahary Karadjov <zahary@gmail.com>
2020-08-15 00:31:09 +0300

    Delegate traceAsyncErrors to Chronicles

--- eth/async_utils.nim
+++ eth/async_utils.nim
@@ -1,23 +1,8 @@
 import
-  chronos, chronicles
+  chronos, chronicles, chronicles/chronos_tools
 
-proc catchOrQuit*(error: Exception) =
-  if error of CatchableError:
-    trace "Async operation ended with a recoverable error", err = error.msg
-  else:
-    fatal "Fatal exception reached", err = error.msg, stackTrace = getStackTrace()
-    quit 1
-
-proc traceAsyncErrors*(fut: FutureBase) =
-  fut.addCallback do (arg: pointer):
-    if not fut.error.isNil:
-      catchOrQuit fut.error[]
-
-template traceAwaitErrors*(fut: FutureBase) =
-  let f = fut
-  yield f
-  if not f.error.isNil:
-    catchOrQuit f.error[]
+export
+  chronos_tools
 
 template awaitWithTimeout*[T](operation: Future[T],
                               deadline: Future[void],
