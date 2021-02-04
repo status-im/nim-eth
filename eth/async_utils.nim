e8c9691b35f4875d9a97193cb965fbf1a956dd7e
Zahary Karadjov <zahary@gmail.com>
2021-02-04 16:45:50 +0200

    awaitWithTimeout for Future[void]

--- eth/async_utils.nim
+++ eth/async_utils.nim
@@ -22,3 +22,21 @@ template awaitWithTimeout*[T](operation: Future[T],
                               timeout: Duration,
                               onTimeout: untyped): T =
   awaitWithTimeout(operation, sleepAsync(timeout), onTimeout)
+
+template awaitWithTimeout*(operation: Future[void],
+                           deadline: Future[void],
+                           onTimeout: untyped) =
+  let f = operation
+  await f or deadline
+  if not f.finished:
+    # If we don't wait for for the cancellation here, it's possible that
+    # the "next" operation will run concurrently to this one, messing up
+    # the order of operations (since await/async is not fair)
+    await cancelAndWait(f)
+    onTimeout
+
+template awaitWithTimeout*(operation: Future[void],
+                           timeout: Duration,
+                           onTimeout: untyped) =
+  awaitWithTimeout(operation, sleepAsync(timeout), onTimeout)
+
