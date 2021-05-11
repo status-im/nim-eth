81f0a56ebdecff3fc97ed9096f5b39919e2a6e3e
kdeme <kim.demey@gmail.com>
2021-05-11 09:37:33 +0200

    Add/update bunch of license headers

--- eth/p2p.nim
+++ eth/p2p.nim
@@ -1,12 +1,9 @@
-#
-#                 Ethereum P2P
-#              (c) Copyright 2018
-#       Status Research & Development GmbH
-#
-#            Licensed under either of
-#  Apache License, version 2.0, (LICENSE-APACHEv2)
-#            MIT license (LICENSE-MIT)
-#
+# nim-eth
+# Copyright (c) 2018-2021 Status Research & Development GmbH
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import
   std/[tables, algorithm, random],
