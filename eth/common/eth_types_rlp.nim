38ef05f05b54247c3af1ed808cb14598085795ac
kdeme <kim.demey@gmail.com>
2024-02-05 18:17:25 +0100

    Fix two bugs in Receipts RLP encoding/decoding
    1. Fix Assertion error when receipt is not a List nor has a single
    byte value. Receiving such garbage data would cause a crash.
    2. Fix decoding of Receipt list by adding the missing Blob
    encapsulation
    
    Also added tests for these scenarios.

--- eth/common/eth_types_rlp.nim
+++ eth/common/eth_types_rlp.nim
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Status Research & Development GmbH
+# Copyright (c) 2022-2024 Status Research & Development GmbH
 # Licensed and distributed under either of
 #   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
@@ -349,33 +349,111 @@ proc append*(w: var RlpWriter, rec: Receipt) =
   w.append(rec.bloom)
   w.append(rec.logs)
 
-proc read*(rlp: var Rlp, T: type Receipt): T =
-  if rlp.isList:
-    result.receiptType = LegacyReceipt
-  else:
-    # EIP 2718
-    let recType = rlp.getByteValue
-    rlp.position += 1
-
-    if recType notin {1, 2, 3}:
-      raise newException(UnsupportedRlpError,
-        "TxType expect 1, 2, or 3 got " & $recType)
-    result.receiptType = ReceiptType(recType)
-
+proc readReceiptLegacy(rlp: var Rlp, receipt: var Receipt) =
+  receipt.receiptType = LegacyReceipt
   rlp.tryEnterList()
   if rlp.isBlob and rlp.blobLen in {0, 1}:
-    result.isHash = false
-    result.status = rlp.read(uint8) == 1
+    receipt.isHash = false
+    receipt.status = rlp.read(uint8) == 1
   elif rlp.isBlob and rlp.blobLen == 32:
-    result.isHash = true
-    result.hash   = rlp.read(Hash256)
+    receipt.isHash = true
+    receipt.hash = rlp.read(Hash256)
   else:
     raise newException(RlpTypeMismatch,
       "HashOrStatus expected, but the source RLP is not a blob of right size.")
 
-  rlp.read(result.cumulativeGasUsed)
-  rlp.read(result.bloom)
-  rlp.read(result.logs)
+  rlp.read(receipt.cumulativeGasUsed)
+  rlp.read(receipt.bloom)
+  rlp.read(receipt.logs)
+
+proc readReceiptTyped(rlp: var Rlp, receipt: var Receipt) =
+  if not rlp.hasData:
+    raise newException(MalformedRlpError,
+      "Receipt expected but source RLP is empty")
+  if not rlp.isSingleByte:
+    raise newException(MalformedRlpError,
+      "ReceiptType byte is out of range, must be 0x00 to 0x7f")
+  let recType = rlp.getByteValue
+  rlp.position += 1
+
+  var txVal: ReceiptType
+  if checkedEnumAssign(txVal, recType):
+    case txVal:
+    of Eip2930Receipt, Eip1559Receipt, Eip4844Receipt:
+      receipt.receiptType = txVal
+    of LegacyReceipt:
+      # The legacy type should not be used here.
+      raise newException(MalformedRlpError,
+        "Invalid ReceiptType: " & $recType)
+  else:
+    raise newException(UnsupportedRlpError,
+      "Unsupported ReceiptType: " & $recType)
+
+  # Note: This currently remains the same as the legacy receipt.
+  rlp.tryEnterList()
+  if rlp.isBlob and rlp.blobLen in {0, 1}:
+    receipt.isHash = false
+    receipt.status = rlp.read(uint8) == 1
+  elif rlp.isBlob and rlp.blobLen == 32:
+    receipt.isHash = true
+    receipt.hash = rlp.read(Hash256)
+  else:
+    raise newException(RlpTypeMismatch,
+      "HashOrStatus expected, but the source RLP is not a blob of right size.")
+
+  rlp.read(receipt.cumulativeGasUsed)
+  rlp.read(receipt.bloom)
+  rlp.read(receipt.logs)
+
+proc read*(rlp: var Rlp, T: type Receipt): T =
+  # Individual receipts are encoded and stored as either `RLP([fields..])`
+  # for legacy receipts, or `Type || RLP([fields..])`. Both of these
+  # encodings are byte sequences. The part after `Type` doesn't have to be
+  # RLP in theory, but all types so far use RLP. EIP-2718 covers this.
+  var receipt: Receipt
+  if rlp.isList:
+    rlp.readReceiptLegacy(receipt)
+  else:
+    rlp.readReceiptTyped(receipt)
+  receipt
+
+proc read*(
+    rlp: var Rlp,
+    T: (type seq[Receipt]) | (type openArray[Receipt])
+  ): seq[Receipt] =
+  # In arrays (sequences), receipts are encoded as either `RLP([fields..])`
+  # for legacy receipts, or `RLP(Type || RLP([fields..]))` for all typed
+  # receipts to date. Spot the extra `RLP(..)` blob encoding, to make it
+  # valid RLP inside a larger RLP. EIP-2976 covers this, "Typed Transactions
+  # over Gossip", although it's not very clear about the blob encoding.
+  #
+  # See also note about transactions above.
+  if not rlp.isList:
+    raise newException(RlpTypeMismatch,
+      "Receipts list expected, but source RLP is not a list")
+
+  var receipts: seq[Receipt]
+  for item in rlp:
+    var receipt: Receipt
+    if item.isList:
+      item.readReceiptLegacy(receipt)
+    else:
+      var rr = rlpFromBytes(rlp.read(Blob))
+      rr.readReceiptTyped(receipt)
+    receipts.add receipt
+
+  receipts
+
+proc append*(
+    rlpWriter: var RlpWriter, receipts: seq[Receipt] | openArray[Receipt]
+  ) =
+  # See above about encoding arrays/sequences of receipts.
+  rlpWriter.startList(receipts.len)
+  for receipt in receipts:
+    if receipt.receiptType == LegacyReceipt:
+      rlpWriter.append(receipt)
+    else:
+      rlpWriter.append(rlp.encode(receipt))
 
 proc read*(rlp: var Rlp, T: type EthTime): T {.inline.} =
   result = EthTime rlp.read(uint64)
