c7c471021167fff234667a9daee4625b4b70ed9b
jangko <jangko128@gmail.com>
2024-02-16 11:43:56 +0700

    Don't auto write p2pProtocol macro expansion to file

--- eth/p2p/p2p_protocol_dsl.nim
+++ eth/p2p/p2p_protocol_dsl.nim
@@ -1005,11 +1005,13 @@ macro emitForSingleBackend(
     peerState.getType, networkState.getType)
 
   result = p.genCode()
-  try:
-    result.storeMacroResult true
-  except IOError:
-    # IO error so the generated nim code might not be stored, don't sweat it.
-    discard
+
+  when defined(p2pProtocolDebug):
+    try:
+      result.storeMacroResult true
+    except IOError:
+      # IO error so the generated nim code might not be stored, don't sweat it.
+      discard
 
 macro emitForAllBackends(backendSyms: typed, options: untyped, body: untyped): untyped =
   let name = $(options[0])
