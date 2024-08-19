b4bb3b6feba54260f83b0b7ad0ed8e9162d5a94d
Jordan Hrycaj <jordan@status.im>
2024-08-19 11:40:23 +0100

    Added unit test using rlpx message that causes this problem to detect

--- tests/rlp/all_tests.nim
+++ tests/rlp/all_tests.nim
@@ -1,4 +1,5 @@
 import
   ./test_api_usage,
   ./test_json_suite,
+  ./test_empty_string,
   ./test_object_serialization
---
+++ tests/rlp/test_empty_string.nim
@@ -0,0 +1,35 @@
+{.used.}
+
+import
+  unittest2,
+  stew/byteutils,
+  ../../eth/rlp
+
+type
+  HelloObj = object
+    version*: uint
+    clientId*: string
+    capabilities*: seq[(string,int)]
+    listenPort*: uint
+    nodeId*: array[64, byte]
+
+const
+  # Some hello message seen on rlpx
+  a = "f84c0580c6c5836574684280b840"
+  b = "8ee5fa75daaf0b96a01162f4e1d23daaa676246ccfa3b4787b09915bae9d66173827704c5b80c235ca32265ff04c36d19a2331173e8ff73880dd200648979bb4"
+
+  hello = HelloObj(
+    version: 5,
+    clientId: "",
+    capabilities: @[("eth", 66)],
+    listenPort: 0,
+    nodeId: hexToByteArray[64](b))
+
+proc suite() =
+  suite "Pathological zero string decoding":
+    test "decode rlpx hello message":
+      # Unless fixed, this check crashes with an `IndexDefect` in the
+      # function `rlp.toString()`
+      check (a & b).hexToSeqByte.decode(HelloObj) == hello
+
+suite()
