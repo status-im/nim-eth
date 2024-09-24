2b4a8cb84794f128182e9edc85cae41352d0b804
Jacek Sieka <jacek@status.im>
2024-09-24 10:40:42 +0200

    remove boehmgc support

--- eth/net/nat.nim
+++ eth/net/nat.nim
@@ -52,7 +52,7 @@ when defined(posix):
 
 # Block all/most signals in the current thread, so we don't interfere with regular signal
 # handling elsewhere.
-proc ignoreSignalsInThread*() =
+proc ignoreSignalsInThread() =
   when defined(posix):
     var signalMask, oldSignalMask: Sigset
 
@@ -60,20 +60,6 @@ proc ignoreSignalsInThread*() =
     if sigfillset(signalMask) != 0:
       echo osErrorMsg(osLastError())
       quit(QuitFailure)
-    when defined(boehmgc):
-      # Turns out Boehm GC needs some signals to deal with threads:
-      # https://www.hboehm.info/gc/debugging.html
-      const
-        SIGPWR = 30
-        SIGXCPU = 24
-        SIGSEGV = 11
-        SIGBUS = 7
-      if sigdelset(signalMask, SIGPWR) != 0 or
-        sigdelset(signalMask, SIGXCPU) != 0 or
-        sigdelset(signalMask, SIGSEGV) != 0 or
-        sigdelset(signalMask, SIGBUS) != 0:
-        echo osErrorMsg(osLastError())
-        quit(QuitFailure)
     if pthread_sigmask(SIG_BLOCK, signalMask, oldSignalMask) != 0:
       echo osErrorMsg(osLastError())
       quit(QuitFailure)
