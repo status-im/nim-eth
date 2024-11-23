eb54399ef737ecb2956c441efdc14985e6f75624
chirag-parmar <chiragparmar12209@gmail.com>
2024-11-23 09:54:29 +0530

    basic rlp profiler

--- tests/rlp/all_tests.nim
+++ tests/rlp/all_tests.nim
@@ -1,7 +1,7 @@
 import
-  ./test_api_usage,
-  ./test_json_suite,
-  ./test_empty_string,
-  ./test_object_serialization,
-  ./test_optional_fields,
+#  ./test_api_usage,
+#  ./test_json_suite,
+#  ./test_empty_string,
+#  ./test_object_serialization,
+#  ./test_optional_fields,
   ./test_rlp_profiler
