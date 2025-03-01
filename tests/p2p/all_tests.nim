00a10652b62a737a7503e9b55d0bb54cb9ec5cdd
jangko <jangko128@gmail.com>
2025-03-01 13:54:55 +0700

    Move p2p code to execution client
    Since consensus layer have it's own networking code and not
    sharing from nim-eth anymore, better move it to execution
    client repo. From there, maintenance and development can
    be easier to suit execution client needs.

--- tests/p2p/all_tests.nim
+++ tests/p2p/all_tests.nim
@@ -1,10 +1,10 @@
+{.used.}
+
 import
-  ./all_discv5_tests,
-  ./test_auth,
-  ./test_crypt,
-  ./test_discovery,
-  ./test_ecies,
-  ./test_enode,
-  ./test_rlpx_thunk,
-  ./test_rlpxtransport,
-  ./test_protocol_handlers
+  ./test_enr,
+  ./test_hkdf,
+  ./test_ip_vote,
+  ./test_routing_table,
+  ./test_discoveryv5_encoding,
+  ./test_discoveryv5_sessions,
+  ./test_discoveryv5
