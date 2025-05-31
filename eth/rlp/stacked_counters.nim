e771122e8c172b436024a975f3c2763a047ebea9
chirag-parmar <chiragparmar12209@gmail.com>
2025-05-31 12:58:19 +0530

    squashed commits

---
+++ eth/rlp/stacked_counters.nim
@@ -0,0 +1,50 @@
+import pkg/results
+
+type
+  StaticStackedCounters*[N: static int, T] = object
+    stack: array[N, tuple[item: T, count: int]]
+    top: int
+
+  DynamicStackedCounters*[T] = object
+    stack: seq[tuple[item: T, count: int]]
+
+proc push*[T](self: var StaticStackedCounters, item: T, count: int) =
+  self.stack[self.top] = (item, count)
+  self.top += 1
+
+proc push*[T](self: var DynamicStackedCounters, item: T, count: int) =
+  self.stack.add((item, count))
+
+proc pop*(self: var StaticStackedCounters, T: type): Opt[T] =
+  if self.top > 0:
+    #decrement the counter
+    self.stack[self.top - 1].count -= 1
+
+    if self.stack[self.top - 1].count == 0:
+      let item = self.stack[self.top - 1].item
+      self.top -= 1
+      return Opt.some(item)
+
+  Opt.none(T)
+
+proc pop*(self: var DynamicStackedCounters, T: type): Opt[T] =
+  let top = self.stack.len
+  if top > 0:
+    #decrement the counter
+    self.stack[top - 1].count -= 1
+
+    if self.stack[top - 1].count == 0:
+      let item = self.stack[top - 1].item
+      self.stack.setLen(top - 1)
+      return Opt.some(item)
+
+  Opt.none(T)
+
+proc init*(self: var DynamicStackedCounters, stackLen: int, T: type) =
+  self.stack = newSeqOfCap[(T, int)](stackLen)
+
+proc clear*(self: var DynamicStackedCounters) =
+  self.stack.setLen(0)
+
+proc clear*(self: var StaticStackedCounters) =
+  self.top = 0
--- eth/rlp/utils.nim
+++ eth/rlp/utils.nim
@@ -5,7 +5,7 @@
 #   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import stew/[bitops2]
+import stew/[bitops2], ./priv/defs
 
 func bytesNeeded*(num: SomeUnsignedInt): int =
   # Number of non-zero bytes in the big endian encoding
@@ -18,3 +18,14 @@ func writeBigEndian*(
   for i in countdown(lastByteIdx, lastByteIdx - numberOfBytes + 1):
     outStream[i] = byte(n and 0xff)
     n = n shr 8
+
+func prefixLength*(dataLen: int): int {.inline.} =
+  return
+    # if dataLen is one byte(more than 127) or more than a byte len but lesser than threshold
+    # length is added to the marker prefix itself - so 1 byte
+    if dataLen < THRESHOLD_LEN:
+      1
+    # if datalen is more than threshold then the length is encoded afte the prefix and
+    # the length of the encoded length is added to marker prefix
+    else:
+      int(uint64(dataLen).bytesNeeded) + 1
